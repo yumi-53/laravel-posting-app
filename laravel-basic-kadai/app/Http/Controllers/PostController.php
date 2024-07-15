4ec9da7fd4c6951382912962a3186a18ae2257e2
yumi-53 <yk53713@gmail.com>
2024-07-15 13:22:24 +0900

    add:Eloquent ORMを使ってデータを取得しよう

--- laravel-basic-kadai/app/Http/Controllers/PostController.php
+++ laravel-basic-kadai/app/Http/Controllers/PostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -14,6 +14,14 @@ class PostController extends Controller
 
         // 変数$postsをposts/index.blade.phpに渡す
         return view('posts.index', compact('posts'));
-
     }
+
+    public function show($id) {
+        // URL'/posts/{$id}'の'{$id}部分と主キー(idカラム）の値が一致するデータをpostsテーブルから取得し変数$postに代入
+        $post = Post::find($id);
+
+        //変数$postをposts/show.blade/phpファイルに渡す
+        return view('posts.show', compact('post'));
+    }
+
 }
